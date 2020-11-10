286ebde3fbbed29964000f2f930731d44ae05555
Menphix Watanabe <internet747@gmail.com>
2020-11-09 23:52:50 -0500

    Adding new script

---
+++ run-exp.py
@@ -0,0 +1,20 @@
+import librosa
+import sys
+import argparse
+import tensorflow as tf
+from tensorflow import keras
+
+def main(args):
+    train_dir = args.train_dir
+    test_dir = args.test_dir
+    print(keras.__version__)
+
+    print("Train dir: {0}".format(train_dir))
+    print("Test dir: {0}".format(test_dir))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train and test the acoustic language-id algorithms.')
+    parser.add_argument('--train-dir', dest='train_dir', action='store', required=True, help='Training directory')
+    parser.add_argument('--test-dir', dest='test_dir', action='store', required=True, help='Testing directory')
+    args = parser.parse_args()
+    sys.exit(main(args))
