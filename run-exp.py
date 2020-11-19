30ae0257ed64b4bdd10df514059a34dfc5323c41
Menphix Watanabe <internet747@gmail.com>
2020-11-19 00:08:48 -0500

    CNN working

--- run-exp.py
+++ run-exp.py
@@ -11,19 +11,21 @@ from tensorflow.keras import models, layers
 # FEAT_LEN = 431  # for 10 seconds of audio, we get 431 MFCC vectors
 FEAT_DIM = 40  # each MFCC vector is of 40 dimensions
 
-def load_train_data(dir, locales, limit_per_locale):
+
+def load_train_data(dir, locales, timesteps, limit_per_locale, channel_last=False):
     '''
     Load the training data from a directory (FLAC files)
     '''
-    sample_file = os.path.join(dir, os.listdir(dir)[0])
-    data, samplerate = sf.read(sample_file)
-    feat = librosa.feature.mfcc(data, sr=samplerate, n_mfcc=FEAT_DIM)
-    feat_len = feat.shape[1]
+    # sample_file = os.path.join(dir, os.listdir(dir)[0])
+    # data, samplerate = sf.read(sample_file)
+    # feat = librosa.feature.mfcc(data, sr=samplerate, n_mfcc=FEAT_DIM)
+    # feat_len = feat.shape[1]
 
-    X = np.zeros([len(locales) * limit_per_locale, feat_len, FEAT_DIM])
-    Y = np.zeros([len(locales) * limit_per_locale, len(locales)])
+    X = np.zeros([len(locales) * limit_per_locale, timesteps, FEAT_DIM])
+    Y_onehot = np.zeros([len(locales) * limit_per_locale, len(locales)])
+    Y = np.zeros(len(locales) * limit_per_locale)
 
-    locales_count = {x:0 for x in locales}
+    locales_count = {x: 0 for x in locales}
     locales_to_idx = dict()
     for i, locale in enumerate(locales):
         locales_to_idx[locale] = i
@@ -38,11 +40,39 @@ def load_train_data(dir, locales, limit_per_locale):
         print("Loading {0} ... ".format(fpath))
         data, samplerate = sf.read(fpath)
         feat = librosa.feature.mfcc(data, sr=samplerate, n_mfcc=FEAT_DIM)
-        X[idx, :, :] = feat.T
-        Y[idx, locales_to_idx[f_locale]] = 1
+
+        # Limiting both X and feat to have timesteps in length
+        feat = feat.T
+        feat_timesteps = feat.shape[0]
+
+        # if feat_timesteps > timesteps:
+        # feat_timesteps = timesteps
+        if feat_timesteps >= timesteps:
+            X[idx, :timesteps, :] = feat[:timesteps, :]
+        else:
+            # feat_timesteps < timesteps
+            remainder = timesteps
+            ridx = 0
+            while feat_timesteps <= remainder:
+                X[idx, ridx * feat_timesteps:(ridx + 1) * feat_timesteps, :] = feat
+                ridx += 1
+                remainder -= feat_timesteps
+            X[idx, ridx * feat_timesteps:, :] = feat[:remainder, :]
+
+            # X[idx, :feat_timesteps, :] = feat.T[:feat_timesteps, :]
+            # remainder = timesteps - feat_timesteps
+            # while remainder > timesteps:
+            #     X[idx, ]
+        Y_onehot[idx, locales_to_idx[f_locale]] = 1
+        Y[idx] = locales_to_idx[f_locale]
         idx += 1
         locales_count[f_locale] += 1
-    return X, Y, locales_to_idx, feat_len
+
+    if channel_last:
+        # Expand one dimension in the last axis
+        X = np.expand_dims(X, axis=3)
+
+    return X, Y, Y_onehot, locales_to_idx
 
 
 def create_rnn_model(input_layer_dim, hidden_layer_dims, output_layer_dim):
@@ -54,45 +84,99 @@ def create_rnn_model(input_layer_dim, hidden_layer_dims, output_layer_dim):
         else:
             return_seq = True
         model.add(layers.LSTM(layer_dim, return_sequences=return_seq))
+        model.add(layers.Dropout(0.2))
     model.add(layers.Dense(output_layer_dim, activation='softmax'))
     # model.add(layers.Softmax(num_outputs))
     return model
 
 
+def create_cnn_model(output_layer_dim):
+    model = models.Sequential()
+    model.add(layers.Conv2D(16, kernel_size=3))
+    model.add(layers.MaxPooling2D())
+    model.add(layers.Conv2D(32, kernel_size=3))
+    model.add(layers.MaxPooling2D())
+    model.add(layers.Conv2D(64, kernel_size=3))
+    model.add(layers.MaxPooling2D())
+    model.add(layers.Flatten())
+    model.add(layers.Dense(100, activation='relu'))
+    model.add(layers.Dense(output_layer_dim, activation='softmax'))
+    return model
+
+
+def eval(predictions, truth):
+    return predictions[predictions == truth].shape[0] / truth.shape[0]
+
+
+def get_model_name(model_type, input_layer_dim, hidden_layer_dims, output_layer_dim, train_data, timesteps, epochs):
+    return "i{0}_h{1}_o{2}_{3}_samples_{4}_epochs_{5}_timesteps_{6}.pb".format(input_layer_dim, '_'.join(
+        [str(x) for x in hidden_layer_dims]), output_layer_dim, train_data, epochs, timesteps, model_type)
+
+
 def main(args):
+    # Training data: 73,080 in total, 24360 per language
+    # Testing data: 540 in total, 180 per language
     train_dir = args.train_dir
     test_dir = args.test_dir
+    save_model_dir = args.save_model_dir
     batch_size = 32
-    epochs = 3
-    model_file = r"C:\Users\menphix\Documents\workspace\build\final-project\models\rnn_model.pb"
+    epochs = 5
+    timesteps = 512
+    input_layer_dim = 100
+    hidden_layer_dims = [100]
+    output_layer_dim = 3
+    num_samples = 24000
+    model_type = 'cnn'
     print(keras.__version__)
 
     print("Train dir: {0}".format(train_dir))
     print("Test dir: {0}".format(test_dir))
     locales = ['en', 'de', 'es']
 
+    # model = create_rnn_model(input_layer_dim, hidden_layer_dims, output_layer_dim)
+    model = create_cnn_model(output_layer_dim)
+
     # train_X shape: [batch, timesteps, feature_dim]
-    # train_X, train_Y, locales_to_idx, train_feat_len = load_train_data(train_dir, locales, 512)
-    # model = create_rnn_model(100, [100], 3)
-    # print(train_X.shape)
-    # print(train_Y.shape)
+    if model_type == 'cnn':
+        channel_last = True
+    else:
+        channel_last = False
+    train_X, train_Y, train_Y_onehot, locales_to_idx = load_train_data(train_dir, locales, timesteps, num_samples,
+                                                                       channel_last=channel_last)
+
+    print(train_X.shape)
+    print(train_Y_onehot.shape)
+    print(train_Y.shape)
     # print(test_X.shape)
     # print(test_Y.shape)
     # print(train_Y)
-    # model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    # history = model.fit(train_X, train_Y, batch_size=batch_size, epochs=epochs)
-    # print(history)
-    # model.save(model_file)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    history = model.fit(train_X, train_Y_onehot, batch_size=batch_size, epochs=epochs)
+    print(history)
 
-    test_X, test_Y, _, test_feat_len = load_train_data(test_dir, locales, 30)
-    model = models.load_model(model_file)
+    test_X, test_Y, test_Y_onehot, _ = load_train_data(test_dir, locales, timesteps, 180, channel_last=channel_last)
+
+    if save_model_dir:
+        model_file = os.path.join(save_model_dir,
+                                  get_model_name(model_type, input_layer_dim, hidden_layer_dims, output_layer_dim,
+                                                 num_samples, timesteps, epochs))
+        print("Saving model to: {0}".format(model_file))
+        model.save(model_file)
+
     predictions = model.predict_classes(test_X)
     print(predictions)
+    print(test_Y)
+    accuracy = eval(predictions, test_Y)
+    print("Accuracy: {0:.2f}%".format(100 * accuracy))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train and test the acoustic language-id algorithms.')
     parser.add_argument('--train-dir', dest='train_dir', action='store', required=True, help='Training directory')
     parser.add_argument('--test-dir', dest='test_dir', action='store', required=True, help='Testing directory')
+    parser.add_argument('--save-model-dir', dest='save_model_dir', action='store', required=False,
+                        help='Directory to save model')
+    # parser.add_argument('--save-feats', dest='save-feats', action='store', required=False, help='Saves the features')
+    # parser.add_argument('--save-feats', dest='save-feats', action='store', required=False, help='Saves the features')
     args = parser.parse_args()
     sys.exit(main(args))
